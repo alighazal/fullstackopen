de3ece6d6bb228fa4e8fa7599cfc2d870cb55eec
alighazal <ali.mdghazal@gmail.com>
2022-10-26 16:13:13 +0300

    exercies 1.6 - complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,7 +8,20 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1> <b> give feedback </b> </h1>
+
+      <div>
+        <button onClick={()=> setGood( good + 1 ) } > good </button>
+        <button onClick={()=> setNeutral( neutral + 1 ) } > neutral </button>
+        <button onClick={()=> setBad( bad + 1 ) }> bad </button>
+      </div>
+
+      <h1> <b> statistics </b> </h1>
+      
+      <div> good { good } </div>
+      <div> neutral { neutral } </div>
+      <div> bad { bad } </div>
+
     </div>
   )
 }
