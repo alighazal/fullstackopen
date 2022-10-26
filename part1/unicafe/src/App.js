97e6877f311d958df7526732fac20e76e0439beb
alighazal <ali.mdghazal@gmail.com>
2022-10-26 16:40:02 +0300

    exercies 1.7-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,48 @@
 import { useState } from 'react'
 
+const Statistics = ({good, bad, neutral}) => {
+
+    const calculateAvg = (good, bad, neutral) => {
+      return ( ((good * 1) + (bad*-1) + (neutral * 0)) / (good + bad + neutral ) );
+    }
+
+    const calculatePositivePercentage = (good, bad, neutral) => {
+      return good / ( good + bad + neutral );
+
+    }
+
+
+  return ( <div>
+      <h1> <b> statistics </b> </h1>
+      
+
+      { 
+      ( good + bad + neutral === 0 ) ? 
+        <p> No Feedback Given </p> :
+        <div>
+          <div> good { good } </div>
+          <div> neutral { neutral } </div>
+          <div> bad { bad } </div>
+
+          
+          <div> all { good + bad + neutral } </div>
+          <div> average { calculateAvg(good, bad, neutral) } </div>
+          <div> positive { calculatePositivePercentage(good, bad, neutral) } % </div>
+        </div>
+      }
+
+     
+  </div>  );
+  
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const calculateAvg = (good, bad, neutral) => {
-    return ( ((good * 1) + (bad*-1) + (neutral * 0)) / (good + bad + neutral ) );
-  }
-
-  const calculatePositivePercentage = (good, bad, neutral) => {
-    return good / ( good + bad + neutral );
-
-  }
-
+ 
   return (
     <div>
       <h1> <b> give feedback </b> </h1>
@@ -25,17 +53,7 @@ const App = () => {
         <button onClick={()=> setBad( bad + 1 ) }> bad </button>
       </div>
 
-      <h1> <b> statistics </b> </h1>
-      
-      <div> good { good } </div>
-      <div> neutral { neutral } </div>
-      <div> bad { bad } </div>
-
-      
-      <div> all { good + bad + neutral } </div>
-      <div> average { calculateAvg(good, bad, neutral) } </div>
-      <div> positive { calculatePositivePercentage(good, bad, neutral) } % </div>
-
+     <Statistics  good={good} bad={bad} neutral={neutral}  />
 
     </div>
   )
