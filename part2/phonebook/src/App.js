40db107dce91e824aaf91638b6cea70836f29fc1
alighazal <ali.mdghazal@gmail.com>
2022-11-04 17:00:37 +0200

    exercise 2.9/2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,63 @@
 import { useState } from 'react'
 
+const Filter = ({filterString, setFilterString}) => {
+
+  return (
+      <div>
+          filter shown with <input  
+                      value={filterString}
+                      onChange={(event) => { setFilterString(event.target.value) } }
+                />
+      </div>
+
+  )
+}
+
+const AddContact = ({addPerson, newPerson, setNewPerson}) => {
+  return (
+      <form  onSubmit={addPerson}>
+        <div>
+          name: <input  
+                      value={newPerson.name}
+                      onChange={(event) => { setNewPerson({...newPerson,
+                                                            name: event.target.value }) } }
+                />
+        </div>
+        <div>number: <input
+                          value={newPerson.number}
+                          onChange={(event) => { setNewPerson({...newPerson,
+                                                      number: event.target.value }) } } /></div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+  )
+}
+
+const Contacts = ({content}) => {
+
+  return (
+    <>
+        {content.map( (person) => <div key={person.number} > {person.name} -- {person.number} </div> )}
+    </>
+  )
+}
+
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0101010101' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newPerson, setNewPerson] = useState({ name: '', number: '' })
+  const [filterString, setFilterString] = useState("")
+
+  const content = persons.filter( (person) => 
+                                        person.name.toLowerCase().search( filterString ) !== -1 )
 
   const addPerson = (event) => {
 
@@ -25,30 +76,20 @@ const App = () => {
     }
   }
 
+
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form  onSubmit={addPerson}>
-        <div>
-          name: <input  
-                      value={newPerson.name}
-                      onChange={(event) => { setNewPerson({...newPerson,
-                                                            name: event.target.value }) } }
-                />
-        </div>
-        <div>number: <input
-                          value={newPerson.number}
-                          onChange={(event) => { setNewPerson({...newPerson,
-                                                      number: event.target.value }) } } /></div>
+      <h1>Phonebook</h1>
 
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filterString={filterString}  setFilterString={setFilterString} />
+
+      <h2>add a new</h2>
+      <AddContact addPerson={addPerson}  newPerson={newPerson} setNewPerson={setNewPerson} /> 
+      
       <h2>Numbers</h2>
-      {
-        persons.map( (person) => <div key={person.number} > {person.name} -- {person.number} </div> )
-      }
+      <Contacts content={content} />
+      
     </div>
   )
 }
