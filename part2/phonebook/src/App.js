860e055ce38b3b6922b1871fb2d5bc2bbd2157a9
alighazal <ali.mdghazal@gmail.com>
2022-11-05 11:38:59 +0200

    exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterString, setFilterString}) => {
 
@@ -47,14 +48,20 @@ const Contacts = ({content}) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState({ name: '', number: '' })
   const [filterString, setFilterString] = useState("")
+
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then( 
+      response => {
+        setPersons(response.data)
+      }
+     )
+  }, [])
 
   const content = persons.filter( (person) => 
                                         person.name.toLowerCase().search( filterString ) !== -1 )
