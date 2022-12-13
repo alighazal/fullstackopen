ec9481b881555d7310ffd2e7b9d245a5e159e3d3
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-13 22:22:01 +0200

    [ex 4.15] creating and fetching users

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,11 +4,13 @@ const app = express()
 const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 app.use(cors())
 app.use(express.json())
 
 app.use ( '/api/blogs' ,blogsRouter)
+app.use ( '/api/users' ,usersRouter)
 
 const errorHandler = (error, request, response, next) => {
 	return response.status(400).json({ 'error': error.message })
