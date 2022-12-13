15bd96871b97236613898c889a739894b6929619
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-14 00:10:52 +0200

    [ ex 4.14 ] token based login

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,12 +5,15 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 app.use(cors())
 app.use(express.json())
 
-app.use ( '/api/blogs' ,blogsRouter)
-app.use ( '/api/users' ,usersRouter)
+app.use ( '/api/blogs', blogsRouter)
+app.use ( '/api/users', usersRouter)
+app.use ( '/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
 	return response.status(400).json({ 'error': error.message })
