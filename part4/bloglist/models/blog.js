ee9ec368b9e999bbd367e25c7da125c041d71c72
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-13 13:00:51 +0200

    [ex 4.2] refactor -- controller

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+	// eslint-disable-next-line no-unused-vars
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+module.exports =  mongoose.model('Blog', blogSchema)
