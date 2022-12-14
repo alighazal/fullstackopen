d0c52f0502abcf6d35258393687e51254e6667d9
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-15 01:19:05 +0200

    [ ex 5.12 ] added eslint

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,36 +1,35 @@
-const BlogForm = ({addBlog, newBlog, setNewBlog }) => (
-    
-    
-    <form onSubmit={addBlog}>
-        <div>
+const BlogForm = ({ addBlog, newBlog, setNewBlog }) => (
+
+  <form onSubmit={addBlog}>
+    <div>
             title
-            <input
-                type="text"
-                value={newBlog.title}
-                name="Username"
-                onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })}
-            />
-        </div>
-        <div>
+      <input
+        type="text"
+        value={newBlog.title}
+        name="Username"
+        onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })}
+      />
+    </div>
+    <div>
             author
-            <input
-                type="text"
-                value={newBlog.author}
-                name="Username"
-                onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })}
-            />
-        </div>
-        <div>
+      <input
+        type="text"
+        value={newBlog.author}
+        name="Username"
+        onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })}
+      />
+    </div>
+    <div>
             url
-            <input
-                type="text"
-                value={newBlog.url}
-                name="Username"
-                onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })}
-            />
-        </div>
-        <button type="submit">save</button>
-    </form>
+      <input
+        type="text"
+        value={newBlog.url}
+        name="Username"
+        onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })}
+      />
+    </div>
+    <button type="submit">save</button>
+  </form>
 )
 
 export default BlogForm
