c9f4b905f19ee04f1a8f57bcc7ec96a817f6be23
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-14 23:35:29 +0200

    [ ex 5.6- 5.7 ] toggle blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+      showDetails?
+        <div style = { blogStyle }>
+          <p>Title: {blog.title}</p>
+          <p>Author: {blog.author}</p>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <button onClick={() => { setShowDetails(!showDetails) }}>{showDetails ? "hide" : "show"}</button>
+        </div >:
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => { setShowDetails(!showDetails) }}>
+          {showDetails ? "hide" : "show"}
+        </button>
+      </div>
+  )
+}
 
 export default Blog
