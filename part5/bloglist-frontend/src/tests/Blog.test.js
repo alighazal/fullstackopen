89f3e9ad292492861ba8a33cdea018c92e0f57e2
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-15 20:09:36 +0200

    [ex 5.14]  blog displayed after 'show' is clicked

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -4,23 +4,46 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
-test('renders title and author & doesn\'t render likes and url', () => {
+describe('Blog', () => {
   let blog = {
     likes: 134,
     author: 'author name ',
     title:' test title',
     url: 'www.test.com'
   }
+  test('renders title and author & doesn\'t render likes and url', () => {
 
-  render(<Blog blog={blog} />)
 
-  const author_displayed = screen.queryByText('author name')
-  const title_displayed = screen.queryByText('test title')
-  const url_displayed = screen.queryByText('www.test.com')
-  const likes_displayed = screen.queryByText('134')
+    render(<Blog blog={blog} />)
 
-  expect(author_displayed).toBeDefined()
-  expect(title_displayed).toBeDefined()
-  expect(url_displayed).toBeNull()
-  expect(likes_displayed).toBeNull()
+    const author_displayed = screen.queryByText('author name')
+    const title_displayed = screen.queryByText('test title')
+    const url_displayed = screen.queryByText('www.test.com')
+    const likes_displayed = screen.queryByText('134')
+
+    expect(author_displayed).toBeDefined()
+    expect(title_displayed).toBeDefined()
+    expect(url_displayed).toBeNull()
+    expect(likes_displayed).toBeNull()
+  })
+
+
+  test('after clicking the button, children are displayed', () => {
+
+    render(<Blog blog={blog} />)
+
+    const button = screen.getByText('show')
+    userEvent.click(button)
+
+    const url_displayed = screen.queryByText('www.test.com')
+    const likes_displayed = screen.queryByText('134')
+    const hide_button_displayed = screen.queryByText('hide')
+
+    expect(url_displayed).toBeDefined()
+    expect(likes_displayed).toBeDefined()
+    expect(hide_button_displayed).toBeDefined()
+  })
+
+
+
 })
