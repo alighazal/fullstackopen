9b886c6b089143c93e83f2d99464fc34f16e8ab9
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-16 00:26:23 +0200

    [ 5.17 ] e2e testing using cypress

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -10,6 +10,12 @@ const blogsRouter = require('./controllers/blogs' )
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
+if (process.env.NODE_ENV === 'test') {
+	const testingRouter = require('./controllers/testing')
+	app.use('/api/testing', testingRouter)
+}
+
+
 app.use(cors())
 app.use(express.json())
 
