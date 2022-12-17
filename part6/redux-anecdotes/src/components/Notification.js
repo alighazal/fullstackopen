458d84551cf88a30f639d0b6c24fd3155373287d
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-17 14:19:42 +0200

    6.19 refactor notif to use connect

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,28 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux' 
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+
+const Notification = (props) => {
+  // const notification = useSelector(state => state.notification)
+
+  console.log(props)
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
   return (
-    notification.message.length !== 0?
+    props.notification.message.length !== 0?
     <div style={style}>
-      {notification.message}
+      {props.notification.message}
     </div>: null
   )
+
 }
 
-export default Notification
+const mapStateToProps = state =>( { notification: state.notification })
+
+export default connect(
+  mapStateToProps,
+  null
+)(Notification)
